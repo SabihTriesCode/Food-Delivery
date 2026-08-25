3cafccda348062285550d42d89a1732d743fabc1
Sabih Khan <lostalgia100l@gmail.com>
2026-08-25 23:52:27 +0500

    delete food and image api added

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,5 +1,9 @@
 import express from "express";
-import { addFood, listFood } from "../controllers/foodController.js";
+import {
+  addFood,
+  listFood,
+  removeFood,
+} from "../controllers/foodController.js";
 import multer from "multer";
 
 const foodRouter = express.Router();
@@ -8,17 +12,15 @@ const foodRouter = express.Router();
 
 const storage = multer.diskStorage({
   destination: "upload",
-  filename:(req,file,cb)=>{
-    return cb(null,`${Date.now()}${file.originalname}`)
+  filename: (req, file, cb) => {
+    return cb(null, `${Date.now()}${file.originalname}`);
+  },
+});
 
-  }
-})
+const upload = multer({ storage: storage });
 
-const upload = multer({storage: storage})
-
-foodRouter.post("/add",upload.single("image"), addFood)
-foodRouter.get("/list",listFood)
-
-
+foodRouter.post("/add", upload.single("image"), addFood);
+foodRouter.get("/list", listFood);
+foodRouter.post("/remove", removeFood);
 
 export default foodRouter;
