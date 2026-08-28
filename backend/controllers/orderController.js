083960439af6abdf581a85ba0e209564a8b1c1df
Sabih Khan <sabihkhan2697@gmail.com>
2026-08-28 23:57:52 +0500

    successfully added payment and verified

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -65,4 +65,46 @@ const placeOrder = async (req, res) => {
   }
 };
 
-export { placeOrder };
+
+const verifyOrder = async (req, res)=> {
+   const {orderId,success} = req.body;
+   try {
+    if(success=="true"){
+      await orderModel.findByIdAndUpdate(orderId, {payment: true})
+      res.json({success: true, message: "Paid"})
+    }
+    else{
+      await orderModel.findByIdAndDelete(orderId)
+      res.json({success: false, message:"Not Paid"})
+    }
+   } catch (error) {
+    console.log(error)
+    res.json({success: false, message:"Error"})
+   }
+}
+
+
+//users order for frontend
+const usersOrder = async (req, res)=> {
+  try {
+    const orders = await orderModel.find({userId: req.body.userId})
+    res.json({success: true, data:orders})
+  } catch (error) {
+    console.log(error)
+    res.json({success: false, message: "Error"})
+  }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+export { placeOrder, verifyOrder, usersOrder };
